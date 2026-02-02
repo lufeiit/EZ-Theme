179ffb1be984e8f75b69efd47452999c7b0ca696
老衲不拈花 <32061921+lufeiit@users.noreply.github.com>
2026-02-02 17:21:40 +0800

    移除文档页面中日期显示的多余空行

--- src/views/docs/DocsPage.vue
+++ src/views/docs/DocsPage.vue
@@ -119,7 +119,7 @@
 
                 <h3 class="doc-title">{{ item.title }}</h3>
 
-                <p class="doc-date">{{ $t('docs.lastUpdated') }}: {{ formatDate(item.updated_at) }}</p>
+                
 
               </div>
 
